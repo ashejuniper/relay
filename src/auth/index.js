817dd29b8e623af3e9571f5f168b14362889df81
Ashelynne Juniper <ashe.juniper@pm.me>
2023-10-31 09:12:12 -0500

    Create index.js

---
+++ src/auth/index.js
@@ -0,0 +1,12 @@
+const { ScarZebraAccount } = require("./ScarZebraAccount.js");
+
+const {
+    getAccountFilePath,
+    identitiesdir
+} = require("./directories.js");
+
+module.exports = {
+    ScarZebraAccount,
+    getAccountFilePath,
+    identitiesdir
+};
